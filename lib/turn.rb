ff6e1363df20401b0dbd370373d301b5e6fedb4d
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-10 19:00:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,12 +40,4 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if !valid_move?(board, index)
-    turn(board)
-  else
-    move(board, index, current_player(board))
-  end
-  display_board(board)
 end
