76ef37960865fa900f203f883f6b2befbdb88f4a
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-10 18:56:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,10 +23,10 @@ end
 
 def valid_move?(board, index)
   if index.between?(0, 8)
-  if !position_taken?(board, index)
-    true
-    end
-    end 
+    if !position_taken?(board, index)
+      true
+  end
+  end
 end
 
 
