ed7b55be0cb674a00b15c39ccf31e17d3a9c0f22
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-10 18:50:35 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,51 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(user_input)
+    user_input.to_i - 1
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || "" || nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+
+def move(board, user_input, character = "X")
+  def update_array_at_with(board, user_input, character)
+      board[user_input] = character
+  end
+  update_array_at_with(board, user_input, character)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if !valid_move?(board, index)
+    turn(board)
+  else
+    move(board, index, current_player(board))
+  end
+  display_board(board)
+end
